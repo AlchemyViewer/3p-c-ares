77f51aa9963d599e0a58211eb2d34eac60e00f87
Rye Mutt <rye@alchemyviewer.org>
2022-01-17 20:49:38 -0500

    Enable linux build

--- build-cmd.sh
+++ build-cmd.sh
@@ -34,59 +34,6 @@ CARES_VERSION_HEADER_DIR="${CARES_SOURCE_DIR}"/include
 version=$(perl -ne 's/#define ARES_VERSION_STR "([^"]+)"/$1/ && print' "${CARES_VERSION_HEADER_DIR}/ares_version.h" | tr -d '\r' )
 echo "${version}" > "${stage}/VERSION.txt"
 
-# Restore all .sos
-restore_sos ()
-{
-    for solib in "${stage}"/packages/lib/release/lib*.so*.disable; do
-        if [ -f "$solib" ]; then
-            mv -f "$solib" "${solib%.disable}"
-        fi
-    done
-}
-
-
-# Restore all .dylibs
-restore_dylibs ()
-{
-    for dylib in "$stage/packages/lib"/release/*.dylib.disable; do
-        if [ -f "$dylib" ]; then
-            mv "$dylib" "${dylib%.disable}"
-        fi
-    done
-}
-
-# Read the version of a particular installable package from autobuild.xml.
-# Optional $2 specifies number of version-number parts to report.
-get_installable_version ()
-{
-    set +x
-    # This command dumps the autobuild.xml data for the specified installable
-    # in Python literal syntax.
-    pydata="$("$autobuild" installables print "$1")"
-    # Now harvest the version key.
-    # It's important to use ''' syntax because of newlines in output. Specify
-    # raw literal syntax too in case of backslashes.
-    # Use ast.literal_eval(), safer than plain builtin eval.
-    # Once we have the Python dict, extract "version" key.
-    # Split version number on '.'.
-    # Keep up to $2 version-number parts.
-    # Rejoin them on '.' again and print.
-    # On Windows, change '\r\n' to plain '\n': the '\r' is NOT removed by
-    # bash, so it becomes part of the string contents, which confuses both
-    # scripted comparisons and human readers.
-    python -c "from ast import literal_eval
-print('.'.join(literal_eval(r'''$pydata''')['version'].split('.')[:${2:-}]))" \
-    | tr -d '\r'
-    set -x
-}
-
-# Given an (e.g. version) string possibly containing periods, escape those
-# periods with backslashes.
-escape_dots ()
-{
-    echo "${1//./\\.}"
-}
-
 pushd "$CARES_SOURCE_DIR"
     case "$AUTOBUILD_PLATFORM" in
         windows*)
@@ -294,143 +241,57 @@ pushd "$CARES_SOURCE_DIR"
             RELEASE_CXXFLAGS="$RELEASE_COMMON_FLAGS -std=c++17"
             DEBUG_CPPFLAGS="-DPIC -DNGHTTP2_STATICLIB"
             RELEASE_CPPFLAGS="-DPIC -D_FORTIFY_SOURCE=2 -DNGHTTP2_STATICLIB"
-            
-            JOBS=`cat /proc/cpuinfo | grep processor | wc -l`
-            
-            # Handle any deliberate platform targeting
-            if [ -z "${TARGET_CPPFLAGS:-}" ]; then
-                # Remove sysroot contamination from build environment
-                unset CPPFLAGS
-            else
-                # Incorporate special pre-processing flags
-                export CPPFLAGS="$TARGET_CPPFLAGS"
-            fi
-            
-            # Force static linkage to libz and openssl by moving .sos out of the way
-            trap restore_sos EXIT
-            for solib in "${stage}"/packages/lib/{release}/lib{z,ssl,crypto}.so*; do
-                if [ -f "$solib" ]; then
-                    mv -f "$solib" "$solib".disable
+            # Debug
+            mkdir -p "build_debug"
+            pushd "build_debug"
+                CFLAGS="$DEBUG_CFLAGS" \
+                CXXFLAGS="$DEBUG_CXXFLAGS" \
+                CPPFLAGS="$DEBUG_CPPFLAGS" \
+                    cmake ../ -G"Ninja" \
+                        -DCMAKE_BUILD_TYPE=Debug \
+                        -DCMAKE_C_FLAGS="$DEBUG_CFLAGS" \
+                        -DCMAKE_CXX_FLAGS="$DEBUG_CXXFLAGS" \
+                        -DCMAKE_INSTALL_PREFIX="$stage" \
+                        -DCMAKE_INSTALL_LIBDIR="$stage/lib/debug" \
+                        -DCARES_BUILD_TESTS=ON \
+                        -DCARES_SHARED=OFF \
+                        -DCARES_STATIC=ON \
+                        -DCARES_STATIC_PIC=ON
+
+                cmake --build . --config Debug --parallel $AUTOBUILD_CPU_COUNT -v
+                cmake --install . --config Debug
+
+                # conditionally run unit tests
+                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                    ctest -C Debug
                 fi
-            done
-            
-            mkdir -p "$stage/lib/release"
-            mkdir -p "$stage/lib/debug"
-            
-            # Fix up path for pkgconfig
-            if [ -d "$stage/packages/lib/release/pkgconfig" ]; then
-                fix_pkgconfig_prefix "$stage/packages"
-            fi
-            
-            OLD_PKG_CONFIG_PATH="${PKG_CONFIG_PATH:-}"
-            
-            # force regenerate autoconf
-            autoreconf -fvi
-            
-            # Autoconf's configure will do some odd things to flags.  '-I' options
-            # will get transferred to '-isystem' and there's a problem with quoting.
-            # Linking and running also require LD_LIBRARY_PATH to locate the OpenSSL
-            # .so's.  The '--with-ssl' option could do this if we had a more normal
-            # package layout.
-            #
-            # configure-time compilation looks like:
-            # ac_compile='$CC -c $CFLAGS $CPPFLAGS conftest.$ac_ext >&5'
-            # ac_link='$CC -o conftest$ac_exeext $CFLAGS $CPPFLAGS $LDFLAGS conftest.$ac_ext $LIBS >&5'
-            saved_path="${LD_LIBRARY_PATH:-}"
-            
-            # Debug configure and build
-            export PKG_CONFIG_PATH="${stage}/packages/lib/debug/pkgconfig:${OLD_PKG_CONFIG_PATH}"
-            export LD_LIBRARY_PATH="${stage}"/packages/lib/debug:"$saved_path"
-            
-            # -g/-O options controled by --enable-debug/-optimize.  Unfortunately,
-            # --enable-debug also defines DEBUGBUILD which changes behaviors.
-            CFLAGS="$DEBUG_CFLAGS" \
-            CXXFLAGS="$DEBUG_CXXFLAGS" \
-            CPPFLAGS="$DEBUG_CPPFLAGS -I$stage/packages/include -I$stage/packages/include/zlib" \
-            LDFLAGS="-L$stage/packages/lib/debug/" \
-            LIBS="-ldl -lpthread" \
-            ./configure --disable-debug --disable-curldebug --disable-optimize --enable-shared=no --enable-threaded-resolver \
-            --enable-cookies --disable-ldap --disable-ldaps  --without-libssh2 \
-            --prefix="\${AUTOBUILD_PACKAGES_DIR}" --libdir="\${prefix}/lib/debug" \
-            --with-ssl="$stage"/packages --with-zlib="$stage"/packages --with-nghttp2="$stage"/packages/
+            popd
 
-            make -j$JOBS
-            make install DESTDIR="$stage"
-            
-            # conditionally run unit tests
-            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-                pushd tests
-                # We hijack the 'quiet-test' target and redefine it as
-                # a no-valgrind test.  Also exclude test 320.  It fails in the
-                # 7.41 distribution with our configuration options.
-                #
-                # Expect problems with the unit tests, they're very sensitive
-                # to environment.
-                make quiet-test TEST_Q='-n !46 !320'
-                popd
-            fi
-            
-            # Run 'curl' as a sanity check. Capture just the first line, which
-            # should have versions of stuff.
-            curlout="$("${stage}"/bin/curl --version | tr -d '\r' | head -n 1)"
-            # With -e in effect, any nonzero rc blows up the script --
-            # so plain 'expr str : pattern' asserts that str contains pattern.
-            # curl version - should be start of line
-            expr "$curlout" : "curl $(escape_dots "$version")" #> /dev/null
-            # libcurl/version
-            expr "$curlout" : ".* libcurl/$(escape_dots "$version")" > /dev/null
-            # OpenSSL/version
-            expr "$curlout" : ".* OpenSSL/$(escape_dots "$(get_installable_version openssl 3)")" > /dev/null
-            # zlib/version
-            expr "$curlout" : ".* zlib/1.2.11.zlib-ng" > /dev/null
-            # nghttp2/versionx
-            expr "$curlout" : ".* nghttp2/$(escape_dots "$(get_installable_version nghttp2 3)")" > /dev/null
-            
-            make clean || true
-            
-            # Release configure and build
-            export PKG_CONFIG_PATH="$stage/packages/lib/release/pkgconfig:${OLD_PKG_CONFIG_PATH}"
-            export LD_LIBRARY_PATH="${stage}"/packages/lib/release:"$saved_path"
-            
-            CFLAGS="$RELEASE_CFLAGS" \
-            CXXFLAGS="$RELEASE_CXXFLAGS" \
-            CPPFLAGS="$RELEASE_CPPFLAGS -I$stage/packages/include -I$stage/packages/include/zlib" \
-            LDFLAGS="-L$stage/packages/lib/release/" \
-            LIBS="-ldl -lpthread" \
-            ./configure --disable-curldebug --disable-debug  --enable-optimize --enable-shared=no --enable-threaded-resolver \
-            --enable-cookies --disable-ldap --disable-ldaps --without-libssh2 \
-            --prefix="\${AUTOBUILD_PACKAGES_DIR}" --libdir="\${prefix}/lib/release" \
-            --with-ssl="$stage"/packages --with-zlib="$stage"/packages --with-nghttp2="$stage"/packages/
+            # Release
+            mkdir -p "build_release"
+            pushd "build_release"
+                CFLAGS="$RELEASE_CFLAGS" \
+                CXXFLAGS="$RELEASE_CXXFLAGS" \
+                CPPFLAGS="$RELEASE_CPPFLAGS" \
+                    cmake ../ -G"Ninja" \
+                        -DCMAKE_BUILD_TYPE=Release \
+                        -DCMAKE_C_FLAGS="$RELEASE_CFLAGS" \
+                        -DCMAKE_CXX_FLAGS="$RELEASE_CXXFLAGS" \
+                        -DCMAKE_INSTALL_PREFIX="$stage" \
+                        -DCMAKE_INSTALL_LIBDIR="$stage/lib/release" \
+                        -DCARES_BUILD_TESTS=ON \
+                        -DCARES_SHARED=OFF \
+                        -DCARES_STATIC=ON \
+                        -DCARES_STATIC_PIC=ON
 
-            make -j$JOBS
-            make install DESTDIR="$stage"
-            
-            # conditionally run unit tests
-            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-                pushd tests
-                make quiet-test TEST_Q='-n !46 !320'
-                popd
-            fi
-            
-            # Run 'curl' as a sanity check. Capture just the first line, which
-            # should have versions of stuff.
-            curlout="$("${stage}"/bin/curl --version | tr -d '\r' | head -n 1)"
-            # With -e in effect, any nonzero rc blows up the script --
-            # so plain 'expr str : pattern' asserts that str contains pattern.
-            # curl version - should be start of line
-            expr "$curlout" : "curl $(escape_dots "$version")" #> /dev/null
-            # libcurl/version
-            expr "$curlout" : ".* libcurl/$(escape_dots "$version")" > /dev/null
-            # OpenSSL/version
-            expr "$curlout" : ".* OpenSSL/$(escape_dots "$(get_installable_version openssl 3)")" > /dev/null
-            # zlib/version
-            expr "$curlout" : ".* zlib/1.2.11.zlib-ng" > /dev/null
-            # nghttp2/version
-            expr "$curlout" : ".* nghttp2/$(escape_dots "$(get_installable_version nghttp2 3)")" > /dev/null
-            
-            make clean || true
-            
-            export LD_LIBRARY_PATH="$saved_path"
+                cmake --build . --config Release --parallel $AUTOBUILD_CPU_COUNT
+                cmake --install . --config Release
+
+                # conditionally run unit tests
+                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                    ctest -C Release
+                fi
+            popd
         ;;
     esac
 
